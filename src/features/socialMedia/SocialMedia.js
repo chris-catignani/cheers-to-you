e5142cb01c983e3955ea7d67e7849a8b0351f0a0
Chris <chris.catignani@gmail.com>
2023-11-25 16:39:26 +0100

    Super basic attempt at TwitterCard

--- src/features/socialMedia/SocialMedia.js
+++ src/features/socialMedia/SocialMedia.js
@@ -1,13 +1,46 @@
 import { Box, Image } from "@chakra-ui/react"
+import { Helmet } from "react-helmet"
 import { useParams } from "react-router-dom"
 
 export const SocialMedia = () => {
-
     const { appId, fileId } = useParams()
 
+    const imageUrl = `https://upcdn.io/${appId}/raw/demo/${fileId}.jpeg`
+
     return (
-        <Box>
-            <Image src={`https://upcdn.io/${appId}/raw/demo/${fileId}.jpeg`}/>
-        </Box>
+        <>
+            <SocialMediaHeader 
+                title={`Cheers2You!`}
+                description={`Celebrate with Cheers2You!`}
+                imageUrl={imageUrl}
+            />
+            <Box>
+                <Image src={imageUrl}/>
+            </Box>
+        </>
+    )
+}
+
+export const SocialMediaHeader = ({title, description, imageUrl}) => {
+    return (
+        <Helmet>
+            { /* Standard metadata tags */ }
+            <title>{title}</title>
+            <meta name='description' content={description} />
+            { /* End standard metadata tags */ }
+
+            { /* Facebook tags */ }
+            {/* <meta property="og:type" content={type} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} /> */}
+            { /* End Facebook tags */ }
+
+            { /* Twitter tags */ }
+            <meta name='twitter:card' content='summary_large_image' />
+            <meta name='twitter:title' content={title} />
+            <meta name='twitter:description' content={description} />
+            <meta name='twitter:image' content={imageUrl} />
+            { /* End Twitter tags */ }
+        </Helmet>
     )
 }
